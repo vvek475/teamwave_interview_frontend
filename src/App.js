710f02715046a08a192378d9bd03324f5acaf555
vivek <vickfreeindia@gmail.com>
2023-02-22 16:49:27 +0530

    modified url

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function App() {
       request_body["intitle"] = inTitle;
     }
 
-    var response_ = await fetch("http://127.0.0.1:8000/", {
+    var response_ = await fetch("https://teamwave.onrender.com/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(request_body),
